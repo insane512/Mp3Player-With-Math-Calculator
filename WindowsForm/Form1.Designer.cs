346abf642442efe6d89c00701281acbbf31512c8
Student-Name <rmendoza1313@gmail.com>
2019-07-09 18:30:43 -0500

    Add project files.

---
+++ WindowsForm/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace WindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txOne = new System.Windows.Forms.TextBox();
+            this.txTwo = new System.Windows.Forms.TextBox();
+            this.lbOut = new System.Windows.Forms.Label();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.btSubtract = new System.Windows.Forms.Button();
+            this.btMultiply = new System.Windows.Forms.Button();
+            this.btDivide = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ckMale = new System.Windows.Forms.CheckBox();
+            this.ckFemale = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btPlay = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.btBrowes = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txOne
+            // 
+            this.txOne.Location = new System.Drawing.Point(234, 115);
+            this.txOne.Name = "txOne";
+            this.txOne.Size = new System.Drawing.Size(100, 20);
+            this.txOne.TabIndex = 2;
+            // 
+            // txTwo
+            // 
+            this.txTwo.Location = new System.Drawing.Point(360, 115);
+            this.txTwo.Name = "txTwo";
+            this.txTwo.Size = new System.Drawing.Size(100, 20);
+            this.txTwo.TabIndex = 3;
+            // 
+            // lbOut
+            // 
+            this.lbOut.AutoSize = true;
+            this.lbOut.Location = new System.Drawing.Point(542, 121);
+            this.lbOut.Name = "lbOut";
+            this.lbOut.Size = new System.Drawing.Size(35, 13);
+            this.lbOut.TabIndex = 4;
+            this.lbOut.Text = "label1";
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(234, 184);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(75, 23);
+            this.btAdd.TabIndex = 5;
+            this.btAdd.Text = "ADD";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.BtAdd_Click);
+            // 
+            // btSubtract
+            // 
+            this.btSubtract.Location = new System.Drawing.Point(234, 225);
+            this.btSubtract.Name = "btSubtract";
+            this.btSubtract.Size = new System.Drawing.Size(75, 23);
+            this.btSubtract.TabIndex = 6;
+            this.btSubtract.Text = "SUBTRACT";
+            this.btSubtract.UseVisualStyleBackColor = true;
+            this.btSubtract.Click += new System.EventHandler(this.BtSubtract_Click);
+            // 
+            // btMultiply
+            // 
+            this.btMultiply.Location = new System.Drawing.Point(234, 274);
+            this.btMultiply.Name = "btMultiply";
+            this.btMultiply.Size = new System.Drawing.Size(75, 23);
+            this.btMultiply.TabIndex = 7;
+            this.btMultiply.Text = "MULTIPLY";
+            this.btMultiply.UseVisualStyleBackColor = true;
+            this.btMultiply.Click += new System.EventHandler(this.BtMultiply_Click);
+            // 
+            // btDivide
+            // 
+            this.btDivide.Location = new System.Drawing.Point(234, 318);
+            this.btDivide.Name = "btDivide";
+            this.btDivide.Size = new System.Drawing.Size(75, 23);
+            this.btDivide.TabIndex = 8;
+            this.btDivide.Text = "DIVIDE";
+            this.btDivide.UseVisualStyleBackColor = true;
+            this.btDivide.Click += new System.EventHandler(this.BtDivide_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(360, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "CLEAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // ckMale
+            // 
+            this.ckMale.AutoSize = true;
+            this.ckMale.Location = new System.Drawing.Point(360, 230);
+            this.ckMale.Name = "ckMale";
+            this.ckMale.Size = new System.Drawing.Size(49, 17);
+            this.ckMale.TabIndex = 10;
+            this.ckMale.Text = "Male";
+            this.ckMale.UseVisualStyleBackColor = true;
+            // 
+            // ckFemale
+            // 
+            this.ckFemale.AutoSize = true;
+            this.ckFemale.Location = new System.Drawing.Point(360, 254);
+            this.ckFemale.Name = "ckFemale";
+            this.ckFemale.Size = new System.Drawing.Size(60, 17);
+            this.ckFemale.TabIndex = 11;
+            this.ckFemale.Text = "Female";
+            this.ckFemale.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(360, 294);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "ENTER";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // btPlay
+            // 
+            this.btPlay.Location = new System.Drawing.Point(536, 318);
+            this.btPlay.Name = "btPlay";
+            this.btPlay.Size = new System.Drawing.Size(75, 23);
+            this.btPlay.TabIndex = 13;
+            this.btPlay.Text = "Play";
+            this.btPlay.UseVisualStyleBackColor = true;
+            this.btPlay.Click += new System.EventHandler(this.BtPlay_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(626, 318);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(75, 23);
+            this.btStop.TabIndex = 14;
+            this.btStop.Text = "STOP";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.BtStop_Click);
+            // 
+            // btBrowes
+            // 
+            this.btBrowes.Location = new System.Drawing.Point(722, 274);
+            this.btBrowes.Name = "btBrowes";
+            this.btBrowes.Size = new System.Drawing.Size(75, 23);
+            this.btBrowes.TabIndex = 15;
+            this.btBrowes.Text = "Browse";
+            this.btBrowes.UseVisualStyleBackColor = true;
+            this.btBrowes.Click += new System.EventHandler(this.BtBrowes_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(545, 274);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btBrowes);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btPlay);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ckFemale);
+            this.Controls.Add(this.ckMale);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btDivide);
+            this.Controls.Add(this.btMultiply);
+            this.Controls.Add(this.btSubtract);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.lbOut);
+            this.Controls.Add(this.txTwo);
+            this.Controls.Add(this.txOne);
+            this.Name = "Form1";
+            this.Text = "MY TEST C SHARP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txOne;
+        private System.Windows.Forms.TextBox txTwo;
+        private System.Windows.Forms.Label lbOut;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Button btSubtract;
+        private System.Windows.Forms.Button btMultiply;
+        private System.Windows.Forms.Button btDivide;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox ckMale;
+        private System.Windows.Forms.CheckBox ckFemale;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btPlay;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.Button btBrowes;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsForm/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+// this is for mp3 player
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm
+{
+    public partial class Form1 : Form
+    {
+        // this is for the mp3 player variables
+        private string _command;
+        private bool isOpen;
+
+        // this is the command for mp3 player (must import system.runtime.interopservices)
+
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string strCommand, StringBuilder strReturn, int returnLength, IntPtr hwmdCallback);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtAdd_Click(object sender, EventArgs e)
+        {
+            string fNumber = txOne.Text;
+            string sNumber = txTwo.Text;
+            int firstNumber;
+            int secondNumber;
+            Int32.TryParse(fNumber, out firstNumber);
+            Int32.TryParse(sNumber, out secondNumber);
+            int s = firstNumber + secondNumber;
+            string sum = Convert.ToString(s);
+            lbOut.Text = "The Sum IS: " + sum;
+
+
+
+        }
+
+        private void BtSubtract_Click(object sender, EventArgs e)
+        {
+            string fNumber = txOne.Text;
+            string sNumber = txTwo.Text;
+            int firstNumber;
+            int secondNumber;
+            Int32.TryParse(fNumber, out firstNumber);
+            Int32.TryParse(sNumber, out secondNumber);
+            int s = firstNumber - secondNumber;
+            string sum = Convert.ToString(s);
+            lbOut.Text = "The Sum IS: " + sum;
+        }
+
+        private void BtMultiply_Click(object sender, EventArgs e)
+        {
+            string fNumber = txOne.Text;
+            string sNumber = txTwo.Text;
+            int firstNumber;
+            int secondNumber;
+            Int32.TryParse(fNumber, out firstNumber);
+            Int32.TryParse(sNumber, out secondNumber);
+            int s = firstNumber * secondNumber;
+            string sum = Convert.ToString(s);
+            lbOut.Text = "The Sum IS: " + sum;
+        }
+
+        private void BtDivide_Click(object sender, EventArgs e)
+        {
+            string fNumber = txOne.Text;
+            string sNumber = txTwo.Text;
+            int firstNumber;
+            int secondNumber;
+            Int32.TryParse(fNumber, out firstNumber);
+            Int32.TryParse(sNumber, out secondNumber);
+            int s = firstNumber / secondNumber;
+            string sum = Convert.ToString(s);
+            lbOut.Text = "The Sum IS: " + sum;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            txOne.Clear();
+            txTwo.Clear();
+            lbOut.Text = " ";
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+                string myFirstText = txOne.Text;
+                string mySecondText = txTwo.Text;
+
+            if (ckMale.Checked)
+            {
+                
+
+                lbOut.Text = "Hello " + myFirstText + " " + mySecondText + " You Are A Male";
+            }
+
+            else if (ckFemale.Checked)
+            {
+                lbOut.Text = "Hello " + myFirstText + " " + mySecondText + " You Are A Female";
+            }
+
+            else { lbOut.Text = "No Box Is Checked!"; }
+
+            
+        }
+
+        // this looks for the mp3 file
+
+        private void BtBrowes_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog mp3File = new OpenFileDialog();
+            mp3File.Filter = "mp3 files|*.mp3|All files (*.*)|*.*";
+            mp3File.FilterIndex = 1;
+
+            if (mp3File.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = mp3File.FileName.ToString();
+
+            }
+
+
+
+
+
+        }
+
+        // this plays the mp3 file
+
+        public void Play(bool loop)
+        {
+
+            if (isOpen)
+            {
+                _command = "play MediaFile";
+
+                if (loop)
+                
+                    _command += "REPEAT";
+                mciSendString(_command, null, 0, IntPtr.Zero);
+
+                
+
+            }
+
+
+
+        }
+
+        // this opens the mp3 player
+
+        public void OpenPlayer(string sFileName)
+        {
+
+            _command = "open \"" + sFileName + "\" type mpegvideo alias MediaFile";
+            mciSendString(_command, null, 0, IntPtr.Zero);
+            isOpen = true;
+        }
+
+        // this closes the mp3 player
+
+        public void ClosePlayer()
+        {
+            _command = "close MediaFile";
+            mciSendString(_command, null, 0, IntPtr.Zero);
+            isOpen = false;
+
+        }
+
+
+        // this is to play the mp3 with button
+
+        private void BtPlay_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                this.OpenPlayer(this.textBox1.Text);
+                this.Play(false);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        // this stops the mp3 from playing
+
+        private void BtStop_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                this.ClosePlayer();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+    }
+}
